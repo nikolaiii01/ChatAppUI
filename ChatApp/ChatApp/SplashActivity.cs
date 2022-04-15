8fb1e8adf3f0ddb02534dd64c06f46075f26ac2d
Nikolai Franz <franztumapon13@gmail.com>
2022-04-15 16:01:51 +0800

    Add project files.

---
+++ ChatApp/ChatApp/SplashActivity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+
+namespace ChatApp
+{
+    [Activity(Label = "ChatApp",Icon = "@mipmap/icon", Theme = "@style/splashscreen", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+    }
+}
